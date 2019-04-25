7fe7305d93010114f1e02648e77e07655d82f6ed
Rajat Khurana <rajat.khurana@nagarro.com>
2019-04-25 12:23:29 +0530

    component input updated

--- src/app/weather-division/weather-division.component.ts
+++ src/app/weather-division/weather-division.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-weather-division',
   templateUrl: './weather-division.component.html',
   styleUrls: ['./weather-division.component.css'],
-  inputs:['parentData']
 })
 export class WeatherDivisionComponent implements OnInit {
+  @Input() parentData: Array<any>
 
   constructor() { }
 
